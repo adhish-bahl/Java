9a0f171199890486241c527b5c6aa91c950a1bb8
Adhish Bahl <adhishbahl0@gmail.com>
2024-01-13 19:53:05 +0530

    Lab 9 done

---
+++ Lab9/Lab9/src/main/java/com/mycompany/lab9/Lab9.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab9;
+
+/**
+ *
+ * @author adhis
+ */
+public class Lab9 {
+
+    public static void main(String[] args) {
+        new Home().setVisible(true);
+    }
+}
